3b46e38c320abaac971bbcf171e2544d9b28c832
Yiyi He <yiyi_he@berkeley.edu>
2025-04-23 18:58:49 -0400

    update news

--- yiyihe-page/src/components/News.jsx
+++ yiyihe-page/src/components/News.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 
 const newsData = [
 	{
+		title: 'Google Guest Lecture',
+		date: '2025/04/03',
+		description: 'Staff AI research scientist Hongxu Ma from Google DeepMind delivered a guest lecture on "The Rise of the Data Science Assistant: LLM Agents in Action".',
+		link: 'https://www.notion.so/How-LLM-Agents-Are-Revolutionizing-Data-Science-Insights-from-Dr-Hongxu-Ma-s-Lecture-1d3a6d2c901280a89136e2ee7bc88a31?pvs=4'
+	},
+	{
 		title: 'BBISS Faculty Fellow',
 		date: '2025/02/19',
 		description: 'Assistant Professor Yiyi He appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS)',
 		link: 'https://research.gatech.edu/seven-appointed-bbiss-faculty-fellows'
 	},
 	{
-		title: 'New Research Paper Published!',
+		title: 'New Research Paper Published',
 		date: '2024/12/11',
-		description: 'Our paper Assessing health care access during flooding in sub-saharan Africa is published in AGU24!',
+		description: 'Our paper Assessing health care access during flooding in sub-saharan Africa is published in AGU24.',
 		link: 'https://www.agu.org/annual-meeting'
 	},
 	{
-		title: 'New Research Paper Published!',
+		title: 'New Research Paper Published',
 		date: '2024/08/01',
-		description: 'Our paper Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience is published on Energy Research & Social Science!',
+		description: 'Our paper Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience is published on Energy Research & Social Science',
 		link: 'https://www.sciencedirect.com/science/article/pii/S2214629624001762'
 	}
 ]
