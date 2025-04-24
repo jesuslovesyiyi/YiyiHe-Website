e3c3bff032624cfe414dda5423a5b298be394a11
Yiyi He <yiyi_he@berkeley.edu>
2025-04-23 20:01:43 -0400

    news updates

--- yiyihe-page/src/components/News.jsx
+++ yiyihe-page/src/components/News.jsx
@@ -7,17 +7,40 @@ const newsData = [
 		description: 'Assistant Professor Yiyi He joined the CURA Leadership Symposium as a discussant for Matthew Gonser, Climate Resilience Officer for Los Angeles County.',
 		link: 'https://resilience.research.gatech.edu/feature/translating-urban-resilience-lessons-cura-symposium'
 	},
+
+	{
+		title: 'Tara AI Safety Initiative',
+		date: '2025/04/04',
+		description: 'Tara Liu is accepted into the AI Safety Initiative (AISI) research program at Georgia Tech, where she is contributing to a research paper on AI interpretability..',
+		link: 'https://www.aisi.dev/'
+	},
+
 	{
 		title: 'Google Guest Lecture',
 		date: '2025/04/03',
 		description: 'Staff AI research scientist Hongxu Ma from Google DeepMind came to the GT campus and delivered a guest lecture on "The Rise of the Data Science Assistant: LLM Agents in Action".',
 		link: 'https://www.notion.so/How-LLM-Agents-Are-Revolutionizing-Data-Science-Insights-from-Dr-Hongxu-Ma-s-Lecture-1d3a6d2c901280a89136e2ee7bc88a31?pvs=4'
 	},
+
+	{
+		title: 'Tara Lavender Celebration',
+		date: '2025/04/01',
+		description: 'Tara Liu is recognized as one of 2025 Lavender Celebration graduates.',
+		link: 'https://belonging.gatech.edu/event/lavender-celebration'
+	},
+
 	{
 		title: 'BBISS Faculty Fellow',
 		date: '2025/02/19',
-		description: 'Assistant Professor Yiyi He appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS)',
+		description: 'Assistant Professor Yiyi He appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS).',
 		link: 'https://research.gatech.edu/seven-appointed-bbiss-faculty-fellows'
+	},
+
+	{
+		title: 'Tingyu AI Safety Fellow',
+		date: '2025/02/01',
+		description: 'Tara Liu is selected as one of seven AI Safety Fellows by AISI, a Georgia Tech community dedicated to ensuring that AI is developed for the benefit of the future.',
+		link: 'https://www.aisi.dev/'
 	},
 
 	{
@@ -36,7 +59,7 @@ const newsData = [
 	{
 		title: 'New Research Paper Published',
 		date: '2024/08/01',
-		description: 'Our paper Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience is published on Energy Research & Social Science',
+		description: 'Our paper Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience is published on Energy Research & Social Science.',
 		link: 'https://www.sciencedirect.com/science/article/pii/S2214629624001762'
 	}
 ]
