66dda8ce781782f578e6889b6cb2d71aaec8548c
Yiyi He <yiyi_he@berkeley.edu>
2025-05-21 15:15:40 -0400

    news update

--- yiyihe-page/src/components/News.jsx
+++ yiyihe-page/src/components/News.jsx
@@ -4,14 +4,14 @@ const newsData = [
 	{
 		title: 'CURA Leadership Symposium',
 		date: '2025/04/08',
-		description: 'Assistant Professor Yiyi He joined the CURA Leadership Symposium as a discussant for Matthew Gonser, Climate Resilience Officer for Los Angeles County.',
+		description: 'Yiyi joined the CURA Leadership Symposium as a discussant for Matthew Gonser, Climate Resilience Officer for Los Angeles County.',
 		link: 'https://resilience.research.gatech.edu/feature/translating-urban-resilience-lessons-cura-symposium'
 	},
 
 	{
 		title: 'Tara AI Safety Initiative',
 		date: '2025/04/04',
-		description: 'Tara Liu is accepted into the AI Safety Initiative (AISI) research program at Georgia Tech, where she is contributing to a research paper on AI interpretability.',
+		description: 'Tara is accepted into the AI Safety Initiative (AISI) research program at Georgia Tech, where she is contributing to a research paper on AI interpretability.',
 		link: 'https://www.aisi.dev/'
 	},
 
@@ -25,28 +25,28 @@ const newsData = [
 	{
 		title: 'Tara Lavender Celebration',
 		date: '2025/04/01',
-		description: 'Tara Liu is recognized as one of 2025 Lavender Celebration graduates.',
+		description: 'Tara is recognized as one of 2025 Lavender Celebration graduates.',
 		link: 'https://belonging.gatech.edu/event/lavender-celebration'
 	},
 
 	{
 		title: 'BBISS Faculty Fellow',
 		date: '2025/02/19',
-		description: 'Assistant Professor Yiyi He appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS).',
+		description: 'Yiyi appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS).',
 		link: 'https://research.gatech.edu/seven-appointed-bbiss-faculty-fellows'
 	},
 
 	{
 		title: 'Tingyu AI Safety Fellow',
 		date: '2025/02/01',
-		description: 'Tara Liu is selected as one of seven AI Safety Fellows by AISI, a Georgia Tech community dedicated to ensuring that AI is developed for the benefit of the future.',
+		description: 'Tara is selected as one of seven AI Safety Fellows by AISI, a Georgia Tech community dedicated to ensuring that AI is developed for the benefit of the future.',
 		link: 'https://www.aisi.dev/'
 	},
 
 	{
 		title: 'Stanford Sustainability Accelerator Grant',
 		date: '2024/12/24',
-		description: 'Assistant Professor Yiyi He joined a Stanford-led team awarded a grant from the Stanford Sustainability Accelerator for their proposal, "Harnessing the Data Revolution to Mitigate Flood Impacts on Vulnerable Communities in Sub-Saharan Africa."',
+		description: 'Yiyi joined a Stanford-led team awarded a grant from the Stanford Sustainability Accelerator for their proposal, "Harnessing the Data Revolution to Mitigate Flood Impacts on Vulnerable Communities in Sub-Saharan Africa."',
 		link: 'https://news.stanford.edu/stories/2025/04/sustainability-accelerator-grants-climate-adaptation-planetary-intelligence?utm_content=httpsnewsstanfordedustories202504sustainabilityacceleratorgrantsclimateadaptationplanetaryintelligence&amp;utm_source=feedotter&amp;utm_medium=email&amp;utm_campaign=int-04-20-2025&amp;mkt_tok=NjYwLVRKQy05ODQAAAGZ92jp5YY7f8ZhPOLayMbpIyR6Ll9vEBq_2DT94Q5gTlGHp-5jhT3noT8ixsCTXkkJ36Ahzb2MNAa_zCXOwZ3le_qafLN5x551o_1il-U'
 	},
 
