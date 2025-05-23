6b0662b4902195a95ccd8528b40ba434a8d2238b
Yiyi He <yiyi_he@berkeley.edu>
2025-05-23 13:56:44 -0400

    update news and research description

--- yiyihe-page/src/components/News.jsx
+++ yiyihe-page/src/components/News.jsx
@@ -6,71 +6,67 @@ const newsData = [
 	{
 		title: 'Leadership certificate',
 		date: '2025/05/21',
-		description: 'Yiyi completed the **Office of Faculty Professional Development Leadership Exploration** certificate program for 2024-2025. [Read More](https://faculty.gatech.edu/2024-2025-ofpd-certificate-programs-faculty)',
+		description: 'Yiyi completed the Office of Faculty Professional Development Leadership Exploration certificate program for 2024-2025.',
 		// link: 'https://faculty.gatech.edu/2024-2025-ofpd-certificate-programs-faculty'
-		link: ''
 	},
 
 	{
 		title: 'CURA Leadership Symposium',
 		date: '2025/04/08',
-		description: 'Yiyi joined the *CURA Leadership Symposium* as a discussant for Matthew Gonser, Climate Resilience Officer for Los Angeles County.',
-		link: 'https://resilience.research.gatech.edu/feature/translating-urban-resilience-lessons-cura-symposium'
+		description: 'Yiyi joined the CURA Leadership Symposium as a discussant for Matthew Gonser, Climate Resilience Officer for Los Angeles County. [[Read more]](https://resilience.research.gatech.edu/feature/translating-urban-resilience-lessons-cura-symposium)',
+		// link: 'https://resilience.research.gatech.edu/feature/translating-urban-resilience-lessons-cura-symposium'
 	},
 
 	{
 		title: 'Tara AI Safety Initiative',
 		date: '2025/04/04',
-		description: 'Tara is accepted into the AI Safety Initiative (AISI) research program at Georgia Tech, where she is contributing to a research paper on AI interpretability.',
-		link: 'https://www.aisi.dev/'
+		description: 'Tara is accepted into the [AI Safety Initiative (AISI)](https://www.aisi.dev/) research program at Georgia Tech, where she is contributing to a research paper on AI interpretability.',
 	},
 
 	{
 		title: 'Google Guest Lecture',
 		date: '2025/04/03',
-		description: 'Staff AI research scientist Hongxu Ma from Google DeepMind came to the GT campus and delivered a guest lecture on "The Rise of the Data Science Assistant: LLM Agents in Action".',
-		link: 'https://www.notion.so/How-LLM-Agents-Are-Revolutionizing-Data-Science-Insights-from-Dr-Hongxu-Ma-s-Lecture-1d3a6d2c901280a89136e2ee7bc88a31?pvs=4'
+		description: 'Staff AI research scientist Hongxu Ma from Google DeepMind came to the GT campus and delivered a guest lecture on "The Rise of the Data Science Assistant: LLM Agents in Action". [[Read more]](https://www.notion.so/How-LLM-Agents-Are-Revolutionizing-Data-Science-Insights-from-Dr-Hongxu-Ma-s-Lecture-1d3a6d2c901280a89136e2ee7bc88a31?pvs=4)',
 	},
 
 	{
 		title: 'Tara Lavender Celebration',
 		date: '2025/04/01',
-		description: 'Tara is recognized as one of 2025 Lavender Celebration graduates.',
-		link: 'https://belonging.gatech.edu/event/lavender-celebration'
+		description: 'Tara is recognized as one of 2025 Lavender Celebration graduates. [[Read more]](https://belonging.gatech.edu/event/lavender-celebration)',
 	},
 
 	{
 		title: 'BBISS Faculty Fellow',
 		date: '2025/02/19',
-		description: 'Yiyi appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS).',
-		link: 'https://research.gatech.edu/seven-appointed-bbiss-faculty-fellows'
+		description: 'Yiyi appointed as faculty fellow to the Brook Byers Institute for Sustainable Systems (BBISS). [[Read more]](https://research.gatech.edu/seven-appointed-bbiss-faculty-fellows)',
 	},
 
 	{
 		title: 'Tingyu AI Safety Fellow',
 		date: '2025/02/01',
-		description: 'Tara is selected as one of seven AI Safety Fellows by AISI, a Georgia Tech community dedicated to ensuring that AI is developed for the benefit of the future.',
-		link: 'https://www.aisi.dev/'
+		description: 'Tara is selected as one of seven AI Safety Fellows by [AISI](https://www.aisi.dev/), a Georgia Tech community dedicated to ensuring that AI is developed for the benefit of the future.',
 	},
 
 	{
 		title: 'Stanford Sustainability Accelerator Grant',
 		date: '2024/12/24',
-		description: 'Yiyi joined a Stanford-led team awarded a grant from the Stanford Sustainability Accelerator for their proposal, "Harnessing the Data Revolution to Mitigate Flood Impacts on Vulnerable Communities in Sub-Saharan Africa."',
-		link: 'https://news.stanford.edu/stories/2025/04/sustainability-accelerator-grants-climate-adaptation-planetary-intelligence?utm_content=httpsnewsstanfordedustories202504sustainabilityacceleratorgrantsclimateadaptationplanetaryintelligence&amp;utm_source=feedotter&amp;utm_medium=email&amp;utm_campaign=int-04-20-2025&amp;mkt_tok=NjYwLVRKQy05ODQAAAGZ92jp5YY7f8ZhPOLayMbpIyR6Ll9vEBq_2DT94Q5gTlGHp-5jhT3noT8ixsCTXkkJ36Ahzb2MNAa_zCXOwZ3le_qafLN5x551o_1il-U'
+		description: 'Yiyi joined a Stanford-led team awarded a grant from the Stanford Sustainability Accelerator for their proposal, "Harnessing the Data Revolution to Mitigate Flood Impacts on Vulnerable Communities in Sub-Saharan Africa." [[Read more]](https://news.stanford.edu/stories/2025/04/sustainability-accelerator-grants-climate-adaptation-planetary-intelligence?utm_content=httpsnewsstanfordedustories202504sustainabilityacceleratorgrantsclimateadaptationplanetaryintelligence&amp;utm_source=feedotter&amp;utm_medium=email&amp;utm_campaign=int-04-20-2025&amp;mkt_tok=NjYwLVRKQy05ODQAAAGZ92jp5YY7f8ZhPOLayMbpIyR6Ll9vEBq_2DT94Q5gTlGHp-5jhT3noT8ixsCTXkkJ36Ahzb2MNAa_zCXOwZ3le_qafLN5x551o_1il-U)',
 	},
 
 	{
 		title: 'New Research Paper Published',
 		date: '2024/12/11',
 		description: 'Our paper Assessing health care access during flooding in sub-saharan Africa is published in AGU24.',
-		link: 'https://www.agu.org/annual-meeting'
 	},
 	{
 		title: 'New Research Paper Published',
 		date: '2024/08/01',
-		description: 'Our paper Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience is published on Energy Research & Social Science.',
-		link: 'https://www.sciencedirect.com/science/article/pii/S2214629624001762'
+		description: 'Our paper [Beyond carbon: Unveiling vulnerabilities of the transportation fuel system for climate resilience](https://www.sciencedirect.com/science/article/pii/S2214629624001762) is published in Energy Research & Social Science.',
+	},
+	{
+		title: 'New Research Paper Published',
+		date: '2024/06/15',
+		description: 'Our paper [Dimension reduction approach for understanding resource-flow resilience to climate change](https://www.nature.com/articles/s42005-024-01664-z) is published in Nature Communications Physics.',
 	}
 ]
 
